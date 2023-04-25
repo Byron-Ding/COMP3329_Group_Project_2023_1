f208902e7726b8ae1a1d1d54ba29c5e931587f50
Byron-Ding <zedong@connect.hku.hk>
2023-04-26 00:07:48 +0800

    增加Member透明按钮，展示成员信息

--- Assets/Scripts/StartUpSetting/Data_StartPage.cs
+++ Assets/Scripts/StartUpSetting/Data_StartPage.cs
@@ -8,6 +8,9 @@ public class Data_StartPage : PersistentSingleton<Data_StartPage>
 
     public string player_name;
 
+    public const string MEMBER_TMPBTN_ORIGIN_COVER_START_PAGE = "Member";
+    public const string MEMBER_TMPBTN_MEMBER_NAME_COVER_START_PAGE = "Cover_StartPage";
+
     // Start is called before the first frame update
     void Start()
     {
