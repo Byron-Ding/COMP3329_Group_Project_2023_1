b77e0e296aa70c9fc831ed4fe1d81290a9f7403d
Byron-Ding <zedong@connect.hku.hk>
2023-04-26 00:21:45 +0800

    优化主界面

--- Assets/Scripts/StartUpSetting/Data_StartPage.cs
+++ Assets/Scripts/StartUpSetting/Data_StartPage.cs
@@ -9,7 +9,7 @@ public class Data_StartPage : PersistentSingleton<Data_StartPage>
     public string player_name;
 
     public const string MEMBER_TMPBTN_ORIGIN_COVER_START_PAGE = "Member";
-    public const string MEMBER_TMPBTN_MEMBER_NAME_COVER_START_PAGE = "Cover_StartPage";
+    public const string MEMBER_TMPBTN_MEMBER_NAME_COVER_START_PAGE = "Members\r\nXu Qingluo\t\t3035908108\r\n\r\nSun Haoyu\t\t3035856044\r\n\r\nZhan Wangjia\t3035844560\r\n\r\nDing Zedong\t\t3035947245\r\n";
 
     // Start is called before the first frame update
     void Start()
